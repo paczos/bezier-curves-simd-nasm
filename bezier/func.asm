b6619038abd7713a17a871e45fd154f652e86e7f
paczopaczos <pawel.paczuski@hotmail.com>
2017-01-08 23:37:12 +0100

    pure xmm calcs, segfault left

--- bezier/func.asm
+++ bezier/func.asm
@@ -9,9 +9,9 @@
 
 
 section .data
-	step_start: dq 0.0
-	step_end: dq 1.0
-	step: dq 0.5
+	step_start: dd 0.0
+	step_end: dd 1.0
+	step: dd 0.01
 	u: dd 0, 0, 0, 0 ; aligned
 	u1: dd 0, 0, 0, 0	;aligned
 
@@ -33,14 +33,25 @@ func:
 	;rdi pointsX
 	;xmm0 pointsX
 	;xmm1 pointsY
-	mov eax, 0
-
-
-	fld qword [inter_iter]	;load inter_iter to st0
+	mov rax, 0
+	;curr interiter into xmm5
+	;step into xmm6
+	;end into xmm7
+	movss xmm5, [step_start]
+	movss xmm6, [step]
+	movss xmm7, [step_end]
+	;broadcast  values
+	shufps xmm5, xmm5, 0h
+	shufps xmm6, xmm6, 0h
+	shufps xmm7, xmm7, 0h
+	movss xmm8, [step_start];second iter
+	movss xmm9, [step_end];for end compare
+	movss xmm10, [step];single step
 interpolate:
-	fld qword [step_end]	; load step_end to st0, inter_iter in st1
-	fcomp			;compare inter_iter st1  with step_end s0 and pop step_end
-	jl end_interpolate	;end of interpolation
+
+	comiss xmm8, xmm9
+after_check:
+	ja end_interpolate	;end of interpolation
 
 	;add eax, 1 ;loop test
 	movaps xmm0, [rdi] ;load points x and y to the xmms
@@ -50,39 +61,17 @@ store_iter:
 	
 
 	;load inter_iter as u to xmm2
-	;ebx -> u
-	mov ebx, [inter_iter]
-	mov dword [u], ebx
-	mov dword [u+4], ebx
-	mov dword [u+8], ebx
-	mov dword [u+12], ebx
-	movups xmm2, [u]
+	;xmm5 u
+	;calc u1
 	
-	
-	;fpu should be empty here
-load:	
-	fld qword [step_end];push 1.0 to the fpu
-	fld qword [inter_iter];push iter to the fpu
+	movss xmm2, [step_end]	;load const 1
+	shufps xmm2, xmm2, 0h 	;broadcast const 1
+	subps xmm2, xmm5	;xmm2 1-u
 
-	fsubp st1, st0
-	fstp qword [sires];store at sires 1-u
-	
-loast:
-	
-	fld qword[inter_iter] ;reload interiter to fpu ;this shit corrupts mem
-	
-	mov ebx, [sires]
-	; mov eax, ebx
-	;prepare u1
-	mov dword [u1], ebx
-	mov dword [u1+4], ebx
-	mov dword [u1+8], ebx
-	mov dword [u1+12], ebx
-	movups xmm3, [u1]
+	;LEGACY xmmm3 u1, xmm2 u	new xmm5 u, xmm2 u1
 
-
-;ebx -> loop counter
-;eax -> points num
+	;ebx -> loop counter
+	;eax -> points num
 	mov ebx, 1
 	mov eax, 4 ;everything is calculated for 4 points
 castelj:
@@ -91,21 +80,21 @@ castelj:
 
 	;we have the data, hardcore sse calcs go here
 	movdqa xmm4, xmm0;copy contents of xmm0 to xmm4 for later
-	mulps xmm0, xmm2 ;x1*u
+	mulps xmm0, xmm5 ;x1*u
 	cvtps2dq xmm0, xmm0
 	psrldq xmm0, 4;srli_si128
 	cvtdq2ps xmm0, xmm0;_mm_cvtepi32_ps
-	mulps xmm4, xmm3
+	mulps xmm4, xmm2
 	addps xmm0, xmm4
 	
 	;do the same for ys
 
 	movdqa xmm4, xmm1
-	mulps xmm1, xmm2 ;x1*u
+	mulps xmm1, xmm5 ;x1*u
 	cvtps2dq xmm1, xmm1
 	psrldq xmm1, 4;srli_si128
 	cvtdq2ps xmm1, xmm1;_mm_cvtepi32_ps
-	mulps xmm4, xmm3
+	mulps xmm4, xmm2
 	addps xmm1, xmm4
 
 	add ebx, 1
@@ -120,9 +109,11 @@ end_castelj:
 	;movdqa [u], xmm0 ;x goes to u
 	;movdqa [u1], xmm1;y goest to u1
 
-	pextrw rax, xmm0, 0 ;x
-	pextrw rbx, xmm1, 0 ;y
+before_conv:
+	pextrw eax, xmm0, 0 ;x
+	pextrw ebx, xmm1, 0 ;y
 
+after_conv:
 	mov qword[u+4], rdi
 	mov rdi, 3
 	imul rbx, rdi
@@ -130,24 +121,26 @@ end_castelj:
 	imul rbx, rcx
 	add rbx, rax
 	add rbx, rdx
-;	mov qword[rbx], 250;seg fault
+
+	mov qword[rbx], 250;seg fault
 
 	;pextrw rax, xmm0, 0
 
 	;ddmo eax, dword [u]
 	;store results in pixarray
 
+	add eax, 1
 
 finaladd:
-	fld qword [step]
-	faddp st1, st0;increase inter_iter
+	addps xmm5, xmm6
+	addss xmm9, xmm10
 	jmp interpolate
 end_interpolate:
 
 	
 
 	 
-	mov eax, [step_end]
+;	mov eax, [step_end]
 
 
 
